1c0c6f383860f06aca7c07c896717d7fe7fe399f
Mariana Cid <cidmariana5@gmail.com>
2020-02-11 11:00:19 -0700

    Actors by show methods started.

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,57 @@
+class Network
+  attr_reader :name, :shows
 
+  def initialize(name)
+    @name = name
+    @shows = []
+    @actors = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  # def main_characters
+  #   @shows.map do |show|
+  #     x = show.characters
+  #       x[]
+  #   require "pry"; binding.pry
+  #
+  # end
+
+  def actors_by_character
+    @shows.map do |show|
+      show.characters.map do |character|
+        character.actor
+        @actors << character.actor
+      end
+    end
+  end
+
+  def actors_by_show
+
+    actors_by_show = {}
+
+    actors_by_show[@shows[0]] = @actors[0]
+require "pry"; binding.pry
+
+    actors_by_show
+  end
+end
+
+
+
+
+
+
+
+
+
+# @shows.reduce({}) do |acc, actor|
+#     acc[@shows] = @shows.map do |show|
+#       show.characters.map do |character|
+#         character.actor
+#       end
+#     end
+# end
+# acc
